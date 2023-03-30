43180586e35167d9f593168de85c0733c853ec56
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-03-30 10:27:41 +0530

    added code

--- ElasticKibanaLoggingVerify/ElasticKibanaLoggingVerify/Program.cs
+++ ElasticKibanaLoggingVerify/ElasticKibanaLoggingVerify/Program.cs
@@ -1,3 +1,7 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
+
 namespace ElasticKibanaLoggingVerify
 {
     public class Program
@@ -5,6 +9,23 @@ namespace ElasticKibanaLoggingVerify
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            //To get environment
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIORNMENT");
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(builder.Configuration["ElasticConfiguration:Uri"]))
+                { 
+                    AutoRegisterTemplate=true,
+                    IndexFormat =$"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".","-")}-{environment?.ToLower().Replace(".","-")}-{DateTime.UtcNow:yyyy-MM}"
+                })
+                .Enrich.WithProperty("Environment",environment)
+                .ReadFrom.Configuration(builder.Configuration)
+                .CreateLogger();
 
             // Add services to the container.
 
