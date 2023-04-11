b753ee6ff89184e8930ad4046df97c7321275bc7
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-04-11 09:28:26 +0530

    added code

--- sample-project/src/app/app.component.ts
+++ sample-project/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  /**
+   *
+   */
+  constructor() {
+   console.log("Hello i am constructor");
+
+  }
   title = 'sample-project';
+  name='My Name is VIkash Verma'
+
+  SayHi(){
+    alert("HI");
+  }
 }
